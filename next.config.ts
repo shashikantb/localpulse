14508959ebb8c13478ed93b3b43847dc7d008d1b
shashikant borgavakar <borgavakarshashikant@gmail.com>
2025-06-28 08:53:33 +0000

    Serve images in next-gen formats Est savings of 140 KiB Image formats li

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -7,6 +9,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
