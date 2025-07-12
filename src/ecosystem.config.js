baac40e8b630c680ca8075831adb58fe30ecd5cc
shashikant borgavakar <borgavakarshashikant@gmail.com>
2025-07-12 08:37:59 +0000

    both are having same JWT_SECRET

---
+++ src/ecosystem.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  apps: [
+    {
+      name: 'localpulse-app',
+      script: 'npm',
+      args: 'start',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '2G', // Restart if it exceeds 2GB
+      env: {
+        NODE_ENV: 'production',
+        // Increase the default heap size for the Node.js process
+        NODE_OPTIONS: '--max-old-space-size=2048',
+      },
+    },
+  ],
+};
