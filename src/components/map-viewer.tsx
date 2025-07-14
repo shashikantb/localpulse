62764b95ffcf61da3de37ae6f97a2b9d1e368fe4
shashikant borgavakar <borgavakarshashikant@gmail.com>
2025-07-14 09:25:07 +0000

    still loading --> issue not resolved

--- src/components/map-viewer.tsx
+++ src/components/map-viewer.tsx
@@ -77,7 +77,7 @@ export default function MapViewer() {
 
   const userIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
