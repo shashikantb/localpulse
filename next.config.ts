75da9ade61c1e9626f6a74ee33e785cd7fdd9e00
shashikantb <borgavakarshashikant@gmail.com>
2025-08-01 00:31:13 +0530

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,37 @@
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  productionBrowserSourceMaps: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/localpulse/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
